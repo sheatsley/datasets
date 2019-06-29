3c5cdd48d422d1fe0c3cc93182519711c50634f8
Ryan Sheatsley <ryan@sheatsley.me>
2019-06-29 11:56:33 -0400

    initial preprocessing code

---
+++ process.py
@@ -0,0 +1,10 @@
+""" Converts datasets to ML-ready normalized and non-normalized numpy arrays """
+
+
+def convert(filepath):
+  """
+  A general function to convert popular machine learning datasets to be machine learning ready but this line is super long so why doesnt pep8
+  """
+
+  int my_var = 3
+  return my_var
