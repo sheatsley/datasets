e1dea3156c97f618ce9baa40c426cecd9a3a80af
Ryan Sheatsley <ryan@sheatsley.me>
2019-07-02 15:13:35 -0400

    scale -> preprocess as the module contains other kinds of preprocessing functions beyond attribute scaling

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,7 +1,16 @@
 """ 
-This module contains different attribute scaling techniques. At present, these
+This module contains different attribute preprocessing techniques. At present, these
 functions are simply scikit-learn wrappers.
 """
+
+
+def encode(x):
+    """
+    Encodes categorical features as a one-hot array
+    """
+    from sklearn.preprocessing import OneHotEncoder
+
+    return OneHotEncoder().fit_transform(x)
 
 
 def rescale(x, minimum=0, maximum=1):
