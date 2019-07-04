7e42bdc3559723a53415e84a987548374c6764b1
Ryan Sheatsley <ryan@sheatsley.me>
2019-07-04 14:27:28 -0400

    handler object that generalizes converting raw datasets to ML-ready

---
+++ utils/handler.py
@@ -0,0 +1,156 @@
+"""
+
+Most datasets are uniquely preprocessed. This module defines a generic handler
+class which handles unique preprocessing requirements flexibly.
+
+"""
+
+
+class Handler:
+
+    # define imports used by the handler
+    import csv
+    import numpy as np
+    import pathlib
+    from preprocess import encode
+    from scale import rescale, normalization, standardization, robust_scale, unit_norm
+
+    def scale(self, x, scheme, options=None, **kwargs):
+        """
+        Scales a dataset based on the scheme (with options)
+        Available options:
+        """
+        if scheme == "all":
+            return [getattr(scale, s)(x) for s in dir(scale) if not s.startswith("_")]
+        else:
+            try:
+                if not options:
+                    return [getattr(scale, scheme)(x)]
+                else:
+                    return [getattr(scale, scheme)(x, *options)]
+            except AttributeError:
+                print(scheme, "not found")
+                return -1
+
+    def load(self, path, onehot=False, header=False, **kwargs):
+        """
+        Loads original dataset into memory
+        """
+        try:
+            with open(path) as f:
+
+                # infer the type
+                ext = path.split(".")[-1]
+                if ext in ("txt", "csv"):
+                    x = list(csv.reader(f, delimiter=","))
+
+                    # strip the header if it exists
+                    if header:
+                        del x[0]
+
+                    # convert categorical attributes to one-hot vectors
+                    if onehot:
+                        x = encode(x)
+
+                    return np.array(x, dtype="float")
+                else:
+                    print(ext, "file format not recognized/supported")
+        except (OSError, e):
+            print(e.strerror)
+            return -1
+
+    def save(self, x, org_path, scheme, test):
+        """
+        Saves dataset arrays as binary files with .npy format
+        """
+
+        # check what scaling functions are currently in the module
+        if scheme == "all":
+            schema = [s for s in dir(scale) if not s.startswith("_")]
+        else:
+            schema = [scheme]
+
+        # every original dataset should have an extension (and no other periods)
+        if test:
+            save_path = "datasets/" + org_path[0].split("/")[1] + "/numpy/"
+            pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+            train = org_path[0].split(".")[0].split("/")[-1]
+            train = org_path[1].split(".")[0].split("/")[-1]
+            for s in schema:
+                np.save(save_path.lower() + train + "_" + s, x)
+                np.save(save_path.lower() + test + "_" + s, x)
+        else:
+            save_path = "datasets/" + org_path.split("/")[1] + "/numpy/"
+            pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
+            dataset = org_path.split(".")[0].split("/")[-1]
+            for s in schema:
+                np.save(save_path.lower() + dataset + "_" + s, x)
+        return 0
+
+    def prep(self, path, opts={}, save=True):
+        """
+        Calls handler functions to prepare a dataset
+        """
+
+        # operate pair-wise if there's a dedicated test set
+        if opts["test"]:
+            x = self.load(path, **opts)
+            x = self.scale(x, **opts)
+            if save:
+                self.save(x, path, opts["scheme"], True)
+
+        # otherwise, go dataset-by-dataset
+        else:
+            for p in path:
+                x = self.load(p, **opts)
+                x = self.scale(x, **opts)
+                if save:
+                    self.save(x, p, opts["scheme"], False)
+        return x
+
+
+if __name__ == "__main__":
+    """
+    Convert and save all available datasets
+    Available options:
+    - test: whether or not a dedicated test set exists
+    - onehot: if categorical features should be converted to onehot vectors
+    - scheme: feature scaling schema (schema listed above)
+    """
+    opts = {
+        "dgd": {
+            "path": (
+                "datasets/dgd/original/FixedObstruction_e6.csv",
+                "datasets/dgd/original/FixedObstruction_e7.csv",
+                "datasets/dgd/original/OriginalDataset_e6.csv",
+                "datasets/dgd/original/RandomObstruction_e6.csv",
+                "datasets/dgd/original/RandomObstruction_e7.csv",
+            ),
+            "test": False,
+            "header": True,
+            "scheme": "all",
+        },
+        "nslkdd": {
+            "path": (
+                "datasets/nslkdd/original/KDDTrain+.txt",
+                "datasets/nslkdd/original/KDDTest+.txt",
+            ),
+            "test": True,
+            "onehot": True,
+            "scheme": "all",
+        },
+        "unswnb15": {
+            "path": (
+                "datasets/original/unswnb15/UNSW_NB15_training-set.csv",
+                "datasets/original/unswnb15/UNSW_NB15_testing-set.csv",
+            ),
+            "test": True,
+            "onehot": True,
+            "scheme": "all",
+        },
+    }
+    handler = Handler()
+    for dataset in opts:
+        handler.prep(opts[dataset])
+        print(dataset, "converted")
+    raise SystemExit(0)
