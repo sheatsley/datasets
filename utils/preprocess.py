2a97c4d9080010aa1867585e211ae8d19f21f9d0
Ryan Sheatsley <ryan@sheatsley.me>
2019-07-07 15:13:28 -0400

    added label encoding function

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,7 +4,7 @@ functions are simply scikit-learn wrappers.
 """
 
 
-def encode(x, features):
+def encode_attributes(x, features):
     """
     Encodes categorical features as a one-hot array
     """
@@ -14,6 +14,26 @@ def encode(x, features):
 
     encoder = ColumnTransformer(
         [("", OneHotEncoder(sparse=False), features)],
+        remainder="passthrough",
+        n_jobs=-1,
+    )
+    if isinstance(x, list):
+        encoder.fit(concatenate(x))
+        return [encoder.transform(x[0]), encoder.transform(x[1])]
+    else:
+        return encoder.fit_transform(x)
+
+
+def encode_labels(x, features):
+    """
+    Encodes categorical features as a one-hot array
+    """
+    from numpy import concatenate
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import LabelEncoder
+
+    encoder = ColumnTransformer(
+        [("", LabelEncoder(), features)],
         remainder="passthrough",
         n_jobs=-1,
     )
