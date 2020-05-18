736d8ddff7a15fd5d79539dd7383bd35b4e67a8c
Ryan Sheatsley <ryan@sheatsley.me>
2020-05-18 12:53:25 -0400

    dataset handler 2.0 refactor

--- utils/handler.py
+++ utils/handler.py
@@ -1,11 +1,10 @@
 """
-
-Most datasets are uniquely preprocessed. This module defines a generic handler
-class which handles unique preprocessing requirements flexibly.
-
+The Handler class supports flexibly preprocessing unique dataset requirements.
+Author: Ryan Sheatsley
+Thu May 14 2020
 """
-import copy
-import csv
+
+import arff
 import numpy as np
 import pathlib
 from preprocess import encode_attributes, encode_labels
@@ -13,148 +12,138 @@ import scale
 
 
 class Handler:
-    def load(self, path, header=False, onehot=False, categorical=[], **kwargs):
+    def load(self, path, header, include):
         """
-        Loads original dataset into memory
+        Loads original dataset into memory.
         """
         try:
             with open(path) as f:
-
-                # infer the type
-                ext = path.split(".")[-1]
-                if ext in ("txt", "csv"):
-                    x = list(csv.reader(f, delimiter=","))
-
-                    # strip the header if it exists
-                    if header:
-                        del x[0]
-                    return np.array((x))
-                else:
-                    print(ext, "file format not recognized/supported")
+                return (
+                    np.array(arff.load(f)["data"], dtype=np.string_)
+                    if "arff" == path.split(".")[-1]
+                    else np.genfromtxt(
+                        f,
+                        delimiter=",",
+                        dtype=np.string_,
+                        skip_header=header,
+                        usecols=include,
+                    )
+                )
         except OSError as e:
-            print(e.strerror)
+            print(path, e.strerror)
             return -1
 
-    def manipulate(self, x, label=False, onehot=False, categorical=[], **kwargs):
+    def preprocess(self, x, onehot=(), preserve=(), **kwargs):
         """
         Manipulates attributes based on arguments
         """
 
-        # convert categorical labels to integers
-        if label:
+        # encode labels as ints & convert relative indicies to absolute, if necessary
+        x = encode_labels(
+            x, [label if label >= 0 else label + x.shape[1] for label in preserve]
+        )
 
-            # translate to absolute if excluded indicies are relative
-            if label < 0:
-                if isinstance(x, list):
-                    label = x[0].shape[1] + label
-                else:
-                    label = x.shape[1] + label
-            x = encode_labels(x, [label])
-
-        # convert categorical attributes to one-hot vectors
+        # encode cateogirical attributes as one-hot vectors (must be sequential)
         if onehot:
-            x = encode_attributes(x, list(categorical))
+            x = encode_attributes(x, onehot)
+        return x.astype(np.float32)
 
-        # UTF-8 is space expensive -- convert to numeric datatype
-        if isinstance(x, list):
-            return [x[0].astype("float64"), x[1].astype("float64")]
-        else:
-            return x.astype("float64")
-
-    def normalize(self, x, scheme, exclude, **kwargs):
+    def normalize(self, x, scheme, preserve, **kwargs):
         """
         Scales a dataset based on the scheme
         """
 
-        # translate to absolute if excluded indicies are relative
-        if isinstance(exclude, int) and exclude < 0:
-            if isinstance(x, list):
-                exclude = set(range(x[0].shape[1], x[0].shape[1] + exclude - 1, -1))
-            else:
-                exclude = set(range(x.shape[1], x.shape[1] + exclude - 1, -1))
+        # compute features that will be normalized and convert any relative indicies to absolute
+        features = tuple(
+            (
+                feature
+                for feature in range(x.shape[1])
+                if feature not in set(p if p >= 0 else p + x.shape[1] for p in preserve)
+            )
+        )
+        try:
+            return (
+                [
+                    getattr(scale, s)(x, features, **kwargs)
+                    for s in dir(scale)
+                    if not s.startswith("_")
+                ]
+                if scheme == "all"
+                else [getattr(scale, scheme)(x, features, **kwargs)]
+            )
+        except AttributeError as e:
+            print(scheme, e.strerror)
+            return -1
 
-        # ignore excluded attributes from any scaling
-        if isinstance(x, list):
-            features = list(set(range(x[0].shape[1])).difference(exclude))
-        else:
-            features = list(set(range(x.shape[1])).difference(exclude))
-        if scheme == "all":
-
-            # deep copy because it could be a list of numpy arrays
-            return [
-                getattr(scale, s)(copy.deepcopy(x), features, **kwargs)
-                for s in dir(scale)
-                if not s.startswith("_")
-            ]
-        else:
-            try:
-                return [getattr(scale, scheme)(copy.deepcopy(x), features, **kwargs)]
-            except AttributeError:
-                print(scheme, "not found")
-                return -1
-
-    def save(self, x, org_path, scheme, test):
+    def save(self, x, path, scheme, test, **kwargs):
         """
         Saves dataset arrays as binary files with .npy format
         """
 
         # check what scaling functions are currently in the module
-        if scheme == "all":
-            schema = [s for s in dir(scale) if not s.startswith("_")]
-        else:
-            schema = [scheme]
+        schema = (
+            [s for s in dir(scale) if not s.startswith("_")]
+            if scheme == "all"
+            else [scheme]
+        )
 
-        # every original dataset should have an extension (and no other periods)
+        # save the datasets (name parsing is reliant on the existence of an extension)
+        base = path[0].split("/")[0] + "/numpy/"
+        datasets = [p.split("/")[-1].split(".")[0] for p in path]
+        pathlib.Path(base).mkdir(parents=True, exist_ok=True)
         if test:
-            save_path = org_path[0].split("/")[0] + "/numpy/"
-            pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
-            train = org_path[0].split("/")[-1].split(".")[0].lower()
-            test = org_path[1].split("/")[-1].split(".")[0].lower()
-            for idx, s in enumerate(schema):
-                np.save(save_path.lower() + train + "_" + s, x[idx][0])
-                np.save(save_path.lower() + test + "_" + s, x[idx][1])
+            [
+                (
+                    np.save(
+                        (base + datasets[0] + "_" + s).lower(),
+                        x[0][idy][: kwargs["size"]],
+                    ),
+                    np.save(
+                        (base + datasets[1] + "_" + s).lower(),
+                        x[0][idy][kwargs["size"] :],
+                    ),
+                )
+                for idy, s in enumerate(schema)
+            ]
         else:
-            save_path = org_path.split("/")[0] + "/numpy/"
-            pathlib.Path(save_path).mkdir(parents=True, exist_ok=True)
-            dataset = org_path.split("/")[-1].split(".")[0].lower()
-            for idx, s in enumerate(schema):
-                np.save(save_path.lower() + dataset + "_" + s, x[idx])
+            [
+                np.save((base + d + "_" + s).lower(), x[idx][idy])
+                for idx, d in enumerate(datasets)
+                for idy, s in enumerate(schema)
+            ]
         return 0
 
     def prep(self, opts, save=True):
         """
         Calls handler functions to prepare a dataset
         """
-
-        # operate pair-wise if there's a dedicated test set
         if opts["test"]:
-            x = []
-            paths = opts["path"]
-            for p in opts["path"]:
-                opts["path"] = p
-                x.append(self.load(**opts))
-            opts["path"] = paths
-            x = self.manipulate(x, **opts)
-            x = self.normalize(x, **opts)
-            if save:
-                self.save(x, opts["path"], opts["scheme"], True)
-
-        # otherwise, go dataset-by-dataset
+            x = [
+                self.normalize(
+                    self.preprocess(
+                        np.concatenate(
+                            [
+                                self.load(path, opts["header"], opts["include"])
+                                for path in opts["path"]
+                            ]
+                        ),
+                        **opts
+                    ),
+                    **opts
+                )
+            ]
         else:
-            paths = opts["path"]
-            norms = opts["norm"]
-            excludes = opts["exclude"]
-            for i in range(len(paths)):
-
-                # set any dataset-specific parameters
-                opts["path"] = paths[i]
-                opts["norm"] = norms[i]
-                opts["exclude"] = excludes[i]
-                x = self.load(**opts)
-                x = self.manipulate(x, **opts)
-                x = self.normalize(x, **opts)
-                if save:
-                    self.save(x, opts["path"], opts["scheme"], False)
+            x = [
+                self.normalize(
+                    self.preprocess(
+                        self.load(path, opts["header"], opts["include"]), **opts
+                    ),
+                    **opts
+                )
+                for path in opts["path"]
+            ]
+        if save:
+            self.save(x, **opts)
         return x
 
 
@@ -162,28 +151,36 @@ if __name__ == "__main__":
     """
     Convert and save all available datasets
     Parameters:
-    - path: path to the dataset to load
-    - test: whether or not a dedicated test set exists
     - header: whether or not a header row exists (which will be removed)
-    - label: index of the label (post-onehot indicies)
-    - onehot: if categorical features should be converted to onehot vectors
-    - categorical: list of categorical indicies to onehot encode
-    - scheme: feature scaling schema (schema listed above)
+    - include: only load the following columns (affects indicies for other parameters)
+    - label: index of the label
     - norm: range of features to be norm'd together for unit_norm scaling scheme
-    - exclude: exclude features from any feature scaling (post-onehot indicies)
+    - onehot: list of categorical features to convert to onehot vectors
+    - path: path to the dataset to load
+    - preserve: do not modify these indicies for any scaling schemes (ie labels)
+    - scheme: feature scaling schema (definitions in scale module)
+    - size: if there is a test set, size is the number of training samples
+    - test: whether or not a dedicated test set exists
     """
 
     handler = Handler()
     opts = {
         "dgd-4": {
-            "path": ("dgd/original/ExpNoObst_124.csv", "dgd/original/ExpObst_124.csv"),
-            "test": False,
             "header": True,
+            "include": tuple(x for x in range(9) if x not in set((6, 7))),
+            "norm": range(4),
+            "path": (
+                "dgd/original/Exp_NoObst_124_current.csv",
+                "dgd/original/Exp_Obst_124.csv",
+            ),
+            "preserve": (-1, -2, -3),
             "scheme": "all",
-            "norm": ((list(range(0, 4)),), (list(range(0, 4)),)),
-            "exclude": ((-2), (-2)),
+            "test": False,
         },
         "dgd-8": {
+            "header": True,
+            "include": tuple(x for x in range(12) if x not in set((8, 9))),
+            "norm": range(8),
             "path": (
                 "dgd/original/FixedObstruction_e6.csv",
                 "dgd/original/FixedObstruction_e7.csv",
@@ -191,39 +188,40 @@ if __name__ == "__main__":
                 "dgd/original/RandomObstruction_e6.csv",
                 "dgd/original/RandomObstruction_e7.csv",
             ),
-            "test": False,
-            "header": True,
+            "preserve": (-1, -2),
             "scheme": "all",
-            "norm": (
-                (list(range(0, 8)),),
-                (list(range(0, 8)),),
-                (list(range(0, 8)),),
-                (list(range(0, 8)), [8], [9]),
-                (list(range(0, 8)), [8], [9]),
-            ),
-            "exclude": ((-2), (-2), (-2), (-2), (-2)),
+            "test": False,
         },
         "nslkdd": {
+            "header": False,
+            "include": tuple(x for x in range(43) if x not in set((42,))),
+            "onehot": (1, 2, 3),
             "path": ("nslkdd/original/KDDTrain+.txt", "nslkdd/original/KDDTest+.txt"),
-            "test": True,
-            "label": (-2),
-            "onehot": True,
-            "categorical": (1, 2, 3),
+            "preserve": (-1,),
             "scheme": "all",
-            "exclude": (-2),
+            "size": 125973,
+            "test": True,
+        },
+        "phishing": {
+            "header": False,
+            "include": tuple(x for x in range(49)),
+            "path": ("phishing/original/Phishing_Legitimate_full.arff",),
+            "preserve": (-1,),
+            "scheme": "all",
+            "test": False,
         },
         "unswnb15": {
+            "header": True,
+            "include": tuple(x for x in range(45) if x not in set((0, 44))),
+            "onehot": (1, 2, 3),
             "path": (
                 "unswnb15/original/UNSW_NB15_training-set.csv",
                 "unswnb15/original/UNSW_NB15_testing-set.csv",
             ),
-            "test": True,
-            "header": True,
-            "label": (-2),
-            "onehot": True,
-            "categorical": (2, 3, 4),
+            "preserve": (-1,),
             "scheme": "all",
-            "exclude": (0, 197, 198),
+            "size": 175341,
+            "test": True,
         },
     }
     for dataset in opts:
