3f7ce48450896d883a2297675705ef95dfa14d05
Ryan Sheatsley <ryan@sheatsley.me>
2020-07-23 12:12:06 -0400

    added nslkdd label correction

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,6 +1,7 @@
 """ 
-This module contains different attribute preprocessing techniques. At present, these
-functions are simply scikit-learn wrappers.
+This module contains different attribute preprocessing techniques. The first
+few functions are simply scikit-learn wrappers, while the others are one-off
+pieces of code to handle any dataset particulars.
 """
 
 
@@ -32,3 +33,31 @@ def encode_labels(x, features):
     encoder = ColumnTransformer([("", OrdinalEncoder(), features)], n_jobs=-1)
     x[:, features] = encoder.fit_transform(x)
     return x
+
+
+def nslkdd():
+    """
+    Transforms the default labels of the NSLKDD from specific attacks to
+    attack categories
+    """
+    from numpy import argwhere, isin, load, save
+    from os import listdir
+
+    # mappings are benign, dos, probe, r2l, and u2r
+    mappings = [
+        [16],
+        [1, 8, 14, 19, 27, 32, 0, 33, 21, 10],
+        [25, 7, 15, 20, 11, 24],
+        [4, 3, 6, 18, 12, 35, 34, 30, 37, 38, 29, 28, 26, 13, 36],
+        [2, 9, 23, 17, 31, 39, 22, 5],
+    ]
+    datasets = listdir("../datasets/nslkdd/numpy/")
+    for dataset in datasets:
+        data = load("../datasets/nslkdd/numpy/" + dataset)
+        indicies = []
+        for new_label, old_label in enumerate(mappings):
+            indicies.append((argwhere(isin(data[:, -1], old_label)), new_label))
+        for index, new_label in indicies:
+            data[index, -1] = new_label
+        save("../datasets/nslkdd/numpy/" + dataset, data)
+    return
