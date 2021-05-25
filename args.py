17a711dd7d644e65f9a564f9a2bc606a19140370
Ryan Sheatsley <ryan@sheatsley.me>
2021-05-25 17:18:35 -0400

    finalize initial args

--- args.py
+++ args.py
@@ -5,6 +5,9 @@ Tue May 25 2021
 """
 import argparse  # Parser for command-line options, arguments and sub-commands
 
+# TODO
+# ensure number of ouputs match number of inputs
+
 
 def parse_args():
     """
@@ -14,17 +17,48 @@ def parse_args():
     :return: command-line arguments
     :rtype: ArgumentParser
     """
-    parser = argparse.ArgumentParser(
+    p = argparse.ArgumentParser(
         description="Retrieves and processes popular machine learning datasets."
     )
 
     # mandatory arguments
-    parser.add_arugment("-v", "--verbose", help="increase verbosity", action="store_true")
+    p.add_argument("dataset", help="dataset to retrieve and process")
 
     # optional arguments
-    parser.add_arugment("-v", "--verbose", help="increase verbosity", action="store_true")
-    parser.add_arugment("--version", help="displays module version", action="version", version='3.0')
-    return parser
+    p.add_argument(
+        "-i",
+        "--include",
+        help="column (or indicies) to keep",
+        nargs="+",
+        metavar="FEATURE",
+    )
+    p.add_argument(
+        "--outdir", help="output directory", type=pathlib.PAth, default="out"
+    )
+    p.add_argument(
+        "-o", "--output", help="output file names", nargs="+", action="append", metavar="DATASET_NAME"
+    )
+    p.add_argument(
+        "-s",
+        "--scheme",
+        help="feature manipulation scheme(s) to apply",
+        nargs="+",
+    )
+
+    # flags
+    p.add_argument(
+        "-a", "--analytics", help="compute basic data analytics", action="store_true"
+    )
+    p.add_argument(
+        "--destupefy",
+        help="cleanup datasets automagically (experimental)",
+        action="store_true",
+    )
+    p.add_argument("-v", "--verbose", help="increase verbosity", action="store_true")
+    p.add_argument(
+        "--version", help="displays module version", action="version", version="3.0"
+    )
+    return p
 
 
 if __name__ == "__main__":
