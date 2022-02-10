77f61e5fe0b11bc34fcada048d3ae116ea5423ea
Ryan Sheatsley <ryan@sheatsley.me>
2022-02-10 16:51:33 -0500

    use adapters directory as a package

---
+++ adapters/__init__.py
@@ -0,0 +1,8 @@
+"""
+Lists available adapters.
+Author: Ryan Sheatsley
+Thu Feb 10 2022
+"""
+import adapters.nslkdd  # https://www.unb.ca/cic/datasets/nsl.html
+
+available = (adapters.nslkdd.NSLKDD,)
