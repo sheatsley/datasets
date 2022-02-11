62e99e6e3982e9bc4e63efe963a7428a9bbe771b
Ryan Sheatsley <ryan@sheatsley.me>
2022-02-11 15:15:36 -0500

    bugfix for adapter packge for inheritance across modules

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -3,6 +3,7 @@ Lists available adapters.
 Author: Ryan Sheatsley
 Thu Feb 10 2022
 """
-import adapters.nslkdd  # https://www.unb.ca/cic/datasets/nsl.html
+import adapters.baseadapter  # Base Adapter class for custom datasets
+import adapters.nslkdd  # Adapter for the NSL-KDD
 
 available = (adapters.nslkdd.NSLKDD,)
