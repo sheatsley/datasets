2628dcb35c671f26d986f79c5b388af7c9b8676b
Ryan Sheatsley <ryan@sheatsley.me>
2023-07-11 12:01:18 -0500

    continue refactor (process requires update to save interface and full implementation of command line for console scripts)

--- mlds/datasets.py
+++ mlds/datasets.py
@@ -3,25 +3,195 @@ This module defines the core for the machine learning datasets repo. It defines
 interfaces for (1) parsing command-line arguments, (2) retrieving datasets, (3)
 applying feature and label transformations, (4) and writing data to disk.
 """
+import argparse
 import pathlib
 import subprocess
 
 import numpy as np
 
+import mlds
 import mlds.adapters as adapters
-import mlds.transform as transform
-import mlds.utilities as utilities
+import mlds.transform as transformations
 
 
-def process(
-    dataset,
-    destupefy,
-    features,
-    labels,
-    names,
-    outdir,
-    schemes,
-):
+class Dataset:
+    """
+    The Dataset class represents the main interface for working with loaded
+    datasets via the load method in this module. Specifically, it: (1) provides
+    an intuitive representation of loaded partitions (including the partitions,
+    the number of samples, and associated metadata), and (2) defines wrappers
+    for casting numpy arrays into popular datatypes used in machine learning
+    (e.g., PyTorch Tensors).
+
+    :func:`__init__`: prepares loaded data
+    :func:`__repr__`: shows useful dataset statistics
+    """
+
+    def __init__(self, name, partitions):
+        """
+        This method instantiates Dataset objects and builds a representation
+        from the paired metadata.
+
+        :param name: dataset name (i.e., filename)
+        :type name: str
+        :param partitions: the loaded data partitions
+        :type partitions: list of tuples of form: (partition, namedtuple)
+        :return: a dataset
+        :rtype: Dataset object
+        """
+        import collections  # Container datatypes
+
+        # set partitions and data
+        for part, data in partitions:
+            setattr(
+                self,
+                part,
+                collections.namedtuple(part.capitalize(), ("data", "labels"))(
+                    data.data, data.labels
+                ),
+            )
+            for field in (f for f in data._fields if f not in {"data", "labels"}):
+                setattr(self, field, getattr(data, field))
+
+        # build representation from name and metadata
+        samples, features, classes = zip(
+            *[
+                (
+                    f"{part}={data.data.shape[0]}",
+                    data.data.shape[1],
+                    len(set(data.labels)),
+                )
+                for part, data in partitions
+            ]
+        )
+        samples = ", ".join(samples)
+        transformations = ", ".join(self.transformations)
+        self.name = (
+            f"{name}(samples=({samples}), features={features[0]}, "
+            f"classes={classes[0]}, transformations=({transformations}))"
+        )
+        return None
+
+    def __repr__(self):
+        """
+        This method returns a string-based representation of useful metadata
+        for debugging.
+
+        :return: dataset statistics
+        :rtype: str
+        """
+        return self.name
+
+
+def command_line():
+    """
+    This function defines the main interface for using this repo via the
+    command line. Specifically, it defines the available arguments, verifies
+    that the parsed arguments are compatible, and calls process to retrieve,
+    transform, and save the dataset.
+
+    :return: command-line arguments
+    :rtype: argparse.Namesapce
+    """
+    parser = argparse.ArgumentParser(
+        description="Retrieves and processes popular machine learning datasets",
+        prog="mlds",
+    )
+    parser.add_argument(
+        "dataset",
+        choices=mlds.__available__,
+        help="Dataset to retrieve and process",
+    )
+    parser.add_argument(
+        "-d",
+        "--data",
+        action="append",
+        choices=(
+            getattr(transformations.Transformer, d)
+            for d in (
+                "minmaxscaler",
+                "onehotencoder",
+                "robustscaler",
+                "standardscaler",
+                "uniformscaler",
+            )
+        ),
+        default=[],
+        help="data transformation(s)to apply",
+        nargs="*",
+        type=lambda d: getattr(transformations.Transformer, d),
+    )
+    parser.add_argument(
+        "-f",
+        "--features",
+        action="append",
+        default=[],
+        help="transformable features (or indicies) (can use 'all')",
+        metavar="FEATURE",
+        nargs="*",
+    )
+    parser.add_argument(
+        "-l",
+        "--labels",
+        default=[],
+        choices=(getattr(transformations.Transformer, d) for d in ("labelencoder",)),
+        help="label transformations(s) to apply",
+        nargs="*",
+        type=lambda d: getattr(transformations.Transformer, d),
+    )
+    parser.add_argument(
+        "--filename",
+        help="output file name(s)",
+        metavar="DATASET_NAME",
+        nargs="*",
+    )
+    parser.add_argument(
+        "--destupefy",
+        action="store_true",
+        help="cleanup datasets automagically (experimental)",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        help="Displays module version",
+        version="4.0",
+    )
+    args = parser.parse_args()
+
+    # set default filenames to the product of data and label transformations
+    if not args.filename:
+        args.filename = (
+            f"{args.dataset}_{dt}_{lt}" for dt in args.data for lt in args.labels
+        )
+
+    # validate that every data transformation has a corresponding feature set
+    if (datalen := len(args.data)) != (featurelen := len(args.features)):
+        parser.error(
+            "Each data transformation must have a corresponding feature set!"
+            f" (Parsed {datalen} data transformations and {featurelen} feature sets)"
+        )
+
+    # validate that every dataset has a corresponding filename
+    if (setlen := datalen * len(args.labels)) != (filenamelen := len(args.filename)):
+        parser.error(
+            "Each output dataset must have a corresponding filename!"
+            f" (Parsed {setlen} output datasets and {filenamelen} filenames)"
+        )
+
+    # print parsed arguments and process the dataset
+    print("Arguments:", ", ".join(f"{a}={v}" for a, v in vars(args).items()))
+    process(
+        dataset=args.dataset,
+        data_transforms=args.data,
+        destupefy=args.destupefy,
+        features=args.features,
+        filenames=args.filename,
+        labels=args.labels,
+    )
+    raise SystemExit(0)
+
+
+def process(dataset, data_transforms, destupefy, features, filenames, label_transforms):
     """
     This function serves as a wrapper for the main interfaces of this repo.
     Specifically, this function: (1) retrieves datasets from the adapter
@@ -30,82 +200,67 @@ def process(
 
     :param dataset: dataset to download
     :type dataset: str
+    :param data_transforms: transformations to apply to the data
+    :type data_transforms: tuple of tuples of Transformer callables
     :param destupefy: whether to clean the data (experimental)
     :type destupefy: bool
     :param features: features to manipulate
     :type features: tuple of tuples of strs
-    :param labels: transformations to apply to the labels
-    :type labels: tuple of tuples of Transformer callables
-    :param names: filenames of the saved datasets
-    :type names: tuple of strs
-    :param schemes: transformations to apply to the data
-    :type schemes: tuple of tuples of Transformer callables
+    :param filenames: filenames of the saved datasets
+    :type filenames: tuple of strs
+    :param labels_transforms: transformations to apply to the labels
+    :type label_transforms: tuple of tuples of Transformer callables
     :return: None
     :rtype: NoneType
     """
 
     # retrieve the dataset and get feature names (to resolve "all" argument)
     print(f"Retrieving {dataset}...")
-    data = getattr(adapters, dataset).retrieve()
-    partition = next(iter(data))
-    feature_names = data[partition]["data"].columns
+    datadict = getattr(adapters, dataset).retrieve()
+    partition = next(iter(datadict))
+    feature_names = datadict[partition]["data"].columns
     print(f"Inferred {len(feature_names)} features from {partition} partition.")
 
     # map "all" keyword to all features except those that are one-hot encoded
     print("Resolving 'all' argument with inferred features...")
-    ohot_features = [
-        feature
-        for scheme, feature_list in zip(schemes, features)
-        if transform.Transformer.onehotencoder in scheme
-        for feature in feature_list
-    ]
-    all_feat = feat_names.difference(ohot_features, sort=False).tolist()
-    features = [all_feat if f == ["all"] else f for f in features]
-
-    # ensure that training preceeds testing to ensure correct transformation fits
-    print("Instantiating Transformer & applying transformations...")
-    parts = (
-        (["train", "test"] + [p for p in data if p not in {"train", "test"}])
-        if all(p in data for p in {"train", "test"})
-        else list(data)
+    all_features = (
+        tuple(feature_names.difference(feature_tuple, sort=False))
+        for transform, feature_tuple in zip(data_transforms, features)
+        if transformations.Transformer.onehotencoder in transform
     )
-    transformer = transform.Transformer(features, labels, schemes)
+    features = (af if of == ("all",) else of for af, of in zip(all_features, features))
 
-    # apply transformations to each parittion
-    for part in parts:
-        print(f"Applying transformations to {dataset} {part} partition...")
-        transformer.apply(*data[part].values(), part != "test")
+    # ensure transformations are only fit to the training set (if applicable)
+    print("Instantiating Transformer & applying transformations...")
+    transformer = transformations.Transformer(
+        data_transforms=data_transforms,
+        features=features,
+        label_transforms=label_transforms,
+    )
+    partitions = list(datadict)
+    if "train" in datadict:
+        has_train = True
+        partitions.sorted(key=lambda p: p != "train")
+    else:
+        has_train = False
 
-        # assemble the transformations (and restore feature names)
-        for (transformed_data, transformed_labels, transformations), name in zip(
-            transformer.export(), names
-        ):
-            # if applicable, destupefy
-            transformed_data, transformed_labels = (
-                transformer.destupefy(
-                    transformed_data, transformed_labels, part != "test"
-                )
-                if destupefy
-                else (transformed_data, transformed_labels)
-            )
+    # fit the transformer to the first partition (e.g., training)
+    for partition in partitions:
+        print(f"Applying transformations to {dataset} {partition} partition...")
+        data, labels = datadict[partition].values()
+        fit = partition == "train" or not has_train
+        transformer.apply(data=data, fit=fit, label=labels)
 
-            # read any relevant metadata
-            metadata = {
-                **transformer.metadata(),
-                **{"orgfshape": orgfshape},
-                **{"transformations": transformations},
+        # export the dataset, optionally destupefy, and compute metadata
+        for (data_t, label_t, x, y), filename in zip(transformer.export(), filenames):
+            x, y = transformer.destupefy(fit=fit, x=x, y=y) if destupefy else (x, y)
+            metadata = transformer.metadata() | {
+                "data transformations": data_t,
+                "label transformations": label_t,
             }
 
-            # save (with analytics, if desired)
-            save(
-                transformed_data,
-                transformed_labels,
-                f"{name}-{part}",
-                metadata,
-                precision=precision,
-                analytics=analytics,
-                outdir=outdir,
-            )
+            # write the dataset to disk
+            save(filename=f"{filename}-{partition}", metadata=metadata, x=x, y=y)
     print(f"{dataset} retrieval, transformation, and export complete!")
     return None
 
