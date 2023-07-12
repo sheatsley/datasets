7938fd20ca95c80d27e57fcf86305362e8006d0e
Ryan Sheatsley <ryan@sheatsley.me>
2023-07-12 16:28:07 -0500

    refactor setup to hardcode git hash as version for non-editable installs

--- setup.py
+++ setup.py
@@ -3,7 +3,34 @@ Build script for Machine Learning Datasets
 Author: Ryan Sheatsley
 Date: Thu Jun 2 2022
 """
+import subprocess
+
 import setuptools
+import setuptools.command.install
+
+
+class Install(setuptools.command.install.install):
+    """
+    This class overrides the default install command so that the __version__
+    attribute of the mlds package is set statically.
+
+    :func:`run`: computes the git hash and saves it to a file
+    """
+
+    def run(self):
+        """
+        This method computes the git hash and saves it to a file.
+
+        :return: None
+        :rtype: Nonetype
+        """
+        version = subprocess.check_output(
+            ("git", "rev-parse", "--short", "HEAD"), text=True
+        ).strip()
+        with open("mlds/VERSION", "w") as f:
+            f.write(f"{version}\n")
+        return super().run()
+
 
 with open("README.md", "r") as f:
     long_description = f.read()
@@ -11,6 +38,7 @@ with open("README.md", "r") as f:
 setuptools.setup(
     author="Ryan Sheatsley",
     author_email="ryan@sheatsley.me",
+    cmdclass=dict(install=Install),
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: BSD License",
@@ -37,8 +65,8 @@ setuptools.setup(
     keywords="machine-learning numpy datasets",
     name="mlds",
     packages=setuptools.find_packages(),
-    package_data={"mlds": ["out/*.pkl"]},
+    package_data={"mlds": ["datasets", "VERSION"]},
     python_requires=">=3.8",
     url="https://github.com/sheatsley/datasets",
-    version="3.3",
+    version="4.0",
 )
