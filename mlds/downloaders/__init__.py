3eb6ea6fa6506cc61eebfc32ad76a11a6ee3eb0a
Ryan Sheatsley <ryan@sheatsley.me>
2023-07-25 10:08:27 -0500

    refactor downloader module init with downloader function

--- mlds/downloaders/__init__.py
+++ mlds/downloaders/__init__.py
@@ -1,18 +1,53 @@
 """
-Lists available adapters.
-Author: Ryan Sheatsley
-Thu Feb 10 2022
+This module lists available datasets and defines the downloader function.
 """
-from mlds.adapters import (
-    cicmalmem2022,  # Adapter for the CIC-MalMem-2022
-    nslkdd,  # Adapter for the NSL-KDD
-    phishing,  # Adapter for the Phishing dataset
-    unswnb15,  # Adapter for the UNSW-NB15
+import requests
+from mlds.downloaders import (
+    cicmalmem2022,
+    cifar10,
+    fashionmnist,
+    mnist,
+    nslkdd,
+    phishing,
+    unswnb15,
 )
 
-available = (
-    cicmalmem2022.CICMalMem2022,
-    nslkdd.NSLKDD,
-    phishing.Phishing,
-    unswnb15.UNSWNB15,
-)
+__all__ = [
+    "cicmalmem2022",
+    "cifar10",
+    "fashionmnist",
+    "nslkdd",
+    "mnist",
+    "phishing",
+    "unswnb15",
+]
+
+
+def download(directory, force, urls):
+    """
+    This function serves as a helper function for all dataset modules by
+    downloading the dataset from a set of URLs. Specifically, it: (1) creates
+    the specified directory (if it doesn't exist), (2) downloads and caches the
+    dataset (if it hasn't already been downloaded or if force is True), and (3)
+    returns the downloaded file.
+
+    :param directory: directory to download the datasets to
+    :type directory: pathlib Path object
+    :param force: redownload the data, even if it exists
+    :type force: bool
+    :param urls: URLs to download the dataset from
+    :type urls: tuple of str
+    :return: dowloaded dataset paired with URLs
+    :rtype: dict
+    """
+    dataset = {}
+    directory.mkdir(parents=True, exist_ok=True)
+    for url in urls:
+        data = directory / url.split("/").pop()
+        if not data.is_file() or force:
+            print(f"Downloading {url} to {directory}...")
+            request = requests.get(url)
+            request.raise_for_status()
+            data.write_bytes(request.content)
+        dataset[url] = data.read_bytes()
+    return dataset
